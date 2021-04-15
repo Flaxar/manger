37fa54736190c261d1ebbbdd7e0b70c6de1ab972
Filip Schwarz <filipschwarz007@gmail.com>
2021-04-15 23:00:37 +0200

    creating tables inside database

--- src/main/java/org/manger/backend/DatabaseController.java
+++ src/main/java/org/manger/backend/DatabaseController.java
@@ -8,14 +8,51 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static java.sql.JDBCType.NULL;
+
 public class DatabaseController {
+    Connection connection;
+    Statement statement;
+
     public DatabaseController() {
         String url = "jdbc:sqlite:database.db";
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection connection = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(url);
+            statement = connection.createStatement();
+            createTables();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private void createTables() throws SQLException {
+        String tableSQL = "CREATE TABLE IF NOT EXISTS \"MANGAS\" (\n" +
+                "\t\"ID\"\tINTEGER NOT NULL UNIQUE,\n" +
+                "\t\"Title\"\tTEXT NOT NULL UNIQUE,\n" +
+                "\t\"URLHeader\"\tTEXT NOT NULL UNIQUE,\n" +
+                "\t\"Type\"\tTEXT NOT NULL,\n" +
+                "\t\"Site\"\tTEXT NOT NULL,\n" +
+                "\tPRIMARY KEY(\"ID\" AUTOINCREMENT)\n" +
+                ")";
+        statement.execute(tableSQL);
+
+        tableSQL = "CREATE TABLE IF NOT EXISTS \"GENRES\" (\n" +
+                "\t\"MangaID\"\tINTEGER NOT NULL,\n" +
+                "\t\"Genre\"\tTEXT NOT NULL\n" +
+                ")";
+        statement.execute(tableSQL);
+
+        tableSQL = "CREATE TABLE IF NOT EXISTS \"CHAPTERS\" (\n" +
+                "\t\"MangaID\"\tINTEGER NOT NULL,\n" +
+                "\t\"Number\"\tINTEGER NOT NULL,\n" +
+                "\t\"Index\"\tINTEGER NOT NULL,\n" +
+                "\t\"Point\"\tINTEGER NOT NULL,\n" +
+                "\t\"Type\"\tTEXT NOT NULL,\n" +
+                "\t\"Date\"\tTEXT NOT NULL,\n" +
+                "\t\"Name\"\tTEXT NOT NULL,\n" +
+                "\t\"URL\"\tTEXT NOT NULL\n" +
+                ")";
+        statement.execute(tableSQL);
     }
 }
